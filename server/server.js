6db08229bbe6924932ebb5c3d1f1229f3efa614b
viviann224 <viviann224@gmail.com>
2021-03-22 15:57:45 -0500

    updated to have application on one proxy and start script on root level of project

--- server/server.js
+++ server/server.js
@@ -9,6 +9,8 @@ const { authMiddleware } = require('./utils/auth');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
+const path = require('path');
+
 // create a new Apollo server and pass in our schema data
 const server = new ApolloServer({
   typeDefs,
@@ -21,6 +23,15 @@ server.applyMiddleware({ app });
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+// Serve up static assets
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../client/build')));
+}
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
 
 db.once('open', () => {
   app.listen(PORT, () => {
